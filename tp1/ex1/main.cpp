7cdefd25b272409548d71822dc8a25b65d96d482
gael-bit <gael.ndibodjo@gmail.com>
2024-01-19 11:37:01 +0100

    tp1 - ex1 - q4

--- tp1/ex1/main.cpp
+++ tp1/ex1/main.cpp
@@ -1,13 +1,16 @@
 #include <iomanip>
 #include <iostream>
+#include <string>
 
 int main()
 {
     std::cout << "Entre ton nom: ";
 
-    char name[20] = "";
-    std::cin >> std::setw(20) >> name;
-    std::cout << "Bonjour Palluche La Falluche !" << std::endl;
+    // char name[20] = ""; // avec un char lorsqu'on lui donne un nom de plus de 20 lettres il n'affiche pas le reste
+    std::string name;
+    std::cin >> name;
+    // std::cout << "Bonjour Palluche La Falluche !" << std::endl;
+    std::cout << "Bonjour " << name << std::endl;
 
     return 0;
 }
